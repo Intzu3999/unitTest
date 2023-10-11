bfcc6e4acd7ac8ba15e116407522e55871dd018b
Intzu3999 <intzucareers@gmail.com>
2023-10-11 20:49:31 +0800

    UI changes

--- unitTest.js
+++ unitTest.js
@@ -1,21 +1,23 @@
+console.log('Version 20231011_2');
+
 function check_build_script() {
-console.log("this is check_build_script function version 3");
+console.log("this is check_build_script function");
 }
 
 function lint() {
-console.log("this is lint function version 3");
+console.log("this is lint function");
 }
 
 function compile() {
-console.log("this is compile function version 3");
+console.log("this is compile function");
 }
 
 function pack() {
-console.log("this is pack function version 3");
+console.log("this is pack function");
 }
 
 function test() {
-console.log("this is test function version 3");
+console.log("this is test function");
 }
 
 
